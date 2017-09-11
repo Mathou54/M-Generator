63cf1fa80644862254c59952315ad481305db009
Mathou54 <matt54425+github.com@gmail.com>
2017-09-11 18:13:13 +0200

    Fix tests.

--- src/app/components/m-generator/m-generator.component.spec.ts
+++ src/app/components/m-generator/m-generator.component.spec.ts
@@ -1,25 +1,27 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MGeneratorComponent} from './m-generator.component';
+import {FormsModule} from '@angular/forms';
 
 describe('MGeneratorComponent', () => {
-  let component: MGeneratorComponent;
-  let fixture: ComponentFixture<MGeneratorComponent>;
+	let component: MGeneratorComponent;
+	let fixture: ComponentFixture<MGeneratorComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [MGeneratorComponent]
-    })
-      .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [FormsModule],
+			declarations: [MGeneratorComponent]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MGeneratorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(MGeneratorComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
